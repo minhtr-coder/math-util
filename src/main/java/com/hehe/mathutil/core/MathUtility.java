93c3ca1afa92e7d1fe22ce4e30e9e07293276c37
minhtr-coder <trichmse172870@fpt.edu.vn>
2025-03-13 16:42:57 +0700

    update main code to use recursion

--- src/main/java/com/hehe/mathutil/core/MathUtility.java
+++ src/main/java/com/hehe/mathutil/core/MathUtility.java
@@ -11,25 +11,36 @@ public class MathUtility {
     // mình không tính quá 20! vi 21! kiểu long chứa ko nổi, sẽ tính sai
     //mình chỉ tính từ 0...20!
     //hàm nhận vào n, trả về n!(n trong đoạn từ [0...20]
+//    public static long getFactorial(int n ) {
+//        //kĩ thuật nhồi con heo đất, ốc nhồi thịt, tiếp chiêu
+//        // kĩ thuật nhân dồn, cộng dồn giá trị
+//
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be in the range [0...20]" );
+//
+//        long result = 1; // bài tổng thì ban đầu = 0
+//        if (n == 0)
+//            return 1; // thoát sớm khi có thể, return ngay khi có thể
+//        // if và for nếu chỉ có 1 lện thì ko cần {... 1 lệnh}
+//        // return sớm thì không cần else luôn
+//
+//        // n = 1 2 3 4 -> nhân dồn vào result = result * 1
+//        for (int i = 1; i <= n ; i++) {
+//            // result = result * i
+//            result *= i; // nhân dồn từ 1 đến n, i tăng từ 1 2 3, dồn vào result
+//
+//        }
+//        return result;
+//    }
     public static long getFactorial(int n ) {
         //kĩ thuật nhồi con heo đất, ốc nhồi thịt, tiếp chiêu
         // kĩ thuật nhân dồn, cộng dồn giá trị
 
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be in the range [0...20]" );
+        if (n == 0 || n == 1)
+            return 1;
+        return n * getFactorial(n - 1);
 
-        long result = 1; // bài tổng thì ban đầu = 0
-        if (n == 0)
-            return 1; // thoát sớm khi có thể, return ngay khi có thể
-        // if và for nếu chỉ có 1 lện thì ko cần {... 1 lệnh}
-        // return sớm thì không cần else luôn
-
-        // n = 1 2 3 4 -> nhân dồn vào result = result * 1
-        for (int i = 1; i <= n ; i++) {
-            // result = result * i
-            result *= i; // nhân dồn từ 1 đến n, i tăng từ 1 2 3, dồn vào result
-
-        }
-        return result;
     }
 }
